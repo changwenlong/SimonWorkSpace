ddf0f78a716b69ac5a0e4c7e69de7a6e0e349e87
changwenlong <simonchwl@gmail.com>
2014-12-16 22:02:57 +0800

    demo 根据http请求地址的不同 给出不同的响应页面

--- express/demo/requestHandlers.js
+++ express/demo/requestHandlers.js
@@ -1,9 +1,15 @@
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Start Page");
+  response.end();
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Upload Page");
+  response.end();
 }
 
 exports.start = start;
