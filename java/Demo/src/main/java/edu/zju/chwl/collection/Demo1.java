82566b39d0de1a23c1d88f843a0d262b578c85ba
changwenlong <simonchwl@gmail.com>
2015-06-29 23:36:45 +0800

    add TIJ code

--- java/Demo/src/main/java/edu/zju/chwl/collection/Demo1.java
+++ java/Demo/src/main/java/edu/zju/chwl/collection/Demo1.java
@@ -6,10 +6,12 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Integer> list1=Arrays.<Integer>asList(1,2,3,4,5);
+		List<Integer> list1=Arrays.asList(1,2,3,4,5);
+		System.out.println(list1.getClass().getName());
 		list1.set(1,99);
 		System.out.println(list1);
-		List<Integer> list2=new LinkedList<Integer>(Arrays.asList(1,2,3,4,5));
+		AbstractList<Integer> list2=new LinkedList<Integer>(Arrays.asList(1,2,3,4,5));
+		System.out.println(list2.getClass().getName());
 		list2.add(2,33);
 		System.out.println(list2);
 	}
